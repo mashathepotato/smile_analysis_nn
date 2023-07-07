c4573bd4270bf4d9b1b69d45269cfabc7df974c9
Masha Baidachna <realcowcitizen@gmail.com>
2023-07-07 20:22:56 +0100

    added files

---
+++ facerecognition.py
@@ -0,0 +1,23 @@
+import cv2
+
+# Load the pre-trained face detection cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Load the image
+# image = cv2.imread("images/minors_welcome.png")
+image = cv2.imread("images/tony_stark.jpg")
+
+# Convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Perform face detection
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+# Draw rectangles around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+# Display the image with rectangles
+cv2.imshow('Face Recognition', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
